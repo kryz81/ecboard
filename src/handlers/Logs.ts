f372f29435734962b6f88e3976849a66781a947d
kryz <k@kryz.dev>
2020-04-23 15:01:56 +0200

    get logs by app name

--- src/handlers/Logs.ts
+++ src/handlers/Logs.ts
@@ -7,7 +7,11 @@ export default class Logs {
   static async details(ctx: Context) {
     const logsService = new LogsService(new AppsService());
     try {
-      ctx.body = await logsService.getErrorLog(ctx.params.appName);
+      const [errorData, outData] = await logsService.getLog(ctx.params.appName);
+      ctx.body = {
+        errorData,
+        outData,
+      };
     } catch (err) {
       ctx.status = 503;
       ctx.body = { msg: 'Cannot get pm2 logs' };
--- src/services/LogsService.ts
+++ src/services/LogsService.ts
@@ -1,16 +1,39 @@
-import pm2 from 'pm2';
+import { createReadStream } from 'fs';
 import AppsService from './AppsService';
 
 export default class LogsService {
   constructor(private appsService: AppsService) {}
 
-  async getErrorLog(appName: string): Promise<string | undefined> {
+  async getLog(appName: string): Promise<string[] | undefined[]> {
     const [process] = await this.appsService.getProcess(appName);
 
     if (!process) {
       throw new Error('Process not found');
     }
 
-    return process.pm2_env?.pm_err_log_path;
+    const errorPath = process.pm2_env?.pm_err_log_path;
+    const outPath = process.pm2_env?.pm_out_log_path;
+
+    const actions = [];
+
+    if (errorPath) {
+      actions.push(this.getData(errorPath));
+    }
+    if (outPath) {
+      actions.push(this.getData(outPath));
+    }
+
+    return await Promise.all(actions);
+  }
+
+  private getData(filePath: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const stream = createReadStream(filePath, 'utf-8');
+      let data = '';
+      stream
+        .on('error', (err) => reject(err))
+        .on('data', (chunk) => data + chunk)
+        .on('end', () => resolve(data));
+    });
   }
 }
